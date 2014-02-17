7f9a31b9eddeb0ae05bbc7af4c7c8b6a16011ab5
Mark Benson <markbenson@exosite.com>
2014-02-17 17:28:04 -0600

    Updated to version 0.02

--- lib/App/Dapper.pm
+++ lib/App/Dapper.pm
@@ -36,11 +36,11 @@ App::Dapper - A publishing platform for static websites
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
