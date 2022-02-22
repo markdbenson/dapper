6ab44f4b6b69cf3d1e82e1dc4adec67d474dc4c9
Mark Benson <mark@jayber.org>
2022-02-22 07:52:03 -0600

    Removed unused CGI request handler

--- lib/App/Dapper/Serve.pm
+++ lib/App/Dapper/Serve.pm
@@ -17,40 +17,9 @@ use Unicode::Normalize;
 
 use Data::Dumper::HTML qw(dumper_html);
 
-use base qw(HTTP::Server::Simple::CGI);
-use HTTP::Server::Simple::Static;
-
-=head2 handle_request
-
-Handle a CGI request
-
-=cut
-
 my $webroot = '.';
 my $dirindex = 'index.html';
  
-sub handle_request {
-    my ($self, $cgi) = @_;
-
-    print "HTTP/1.0 200 OK\r\n";
-    print $cgi->header,
-	  $cgi->start_html('Debug'),
-          $cgi->h1("Path: $cgi->path()\n"),
-          dumper_html($cgi),
-	  $cgi->end_html;
-
-    #$webroot =~ s/(.*\/)$/$1$dirindex/;
-
-    if (!$self->serve_static($cgi, $webroot)) {
-        print "HTTP/1.0 404 Not found\r\n";
-        print $cgi->header, 
-              $cgi->start_html('Not found'),
-              $cgi->h1('Not found'),
-              $cgi->h1($webroot),
-              $cgi->end_html;
-    }
-} 
-
 sub kyloren {
     my ($self, $path) = @_;
 
